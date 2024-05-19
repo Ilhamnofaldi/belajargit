41248fb7740fd4b84d6a786a742a7a2499d64bc9
Ilham Nofaldi <81660180+Ilhamnof@users.noreply.github.com>
2024-05-19 16:07:18 +0700

    isi js

---
+++ index.js
@@ -0,0 +1,5 @@
+//buat animasi untuk login js
+const login = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const loginBtn = document.querySelector('.login-btn');
+const loginClose = document.querySelector('.login-close');
